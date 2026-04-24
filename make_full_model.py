fa1570a63f540d00c94efa3e758b77941f20a690
mcosens <mcosens@carnegiescience.edu>
2026-04-24 09:38:13 -0700

    output format now .zmx

--- make_full_model.py
+++ make_full_model.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     TheApplication = zos.TheApplication
     
     # Setup each file
-    IFU_System.LoadFile(r'C:\Users\mcosens\Documents\Zemax\MIRMOS\IFU\virtualIFU_Kband_4-2-26.zmx', False)
+    IFU_System.LoadFile(r'C:\Users\mcosens\Documents\Zemax\MIRMOS\IFU\virtualIFU_Kband_4-2-26.zmx', False) #old format of lens file
     Full_System=TheApplication.LoadNewSystem(r'C:\Users\mcosens\Documents\Zemax\MIRMOS\IFU\MIRMOS_full.zmx')
     #print("Surfaces in IFU system: " + str(IFU_System.LDE.NumberOfSurfaces)) #for testing that these are not being overwritten
     #print("Surfaces in full system: " + str(Full_System.LDE.NumberOfSurfaces))
@@ -59,7 +59,7 @@ if __name__ == '__main__':
             current_op.GetOperandCell(j+1).Value = IFS_op.GetOperandCell(1).Value
     
     ##add configurations with parameters for each slice, repeat for each band
-    for k in range(1):#bands):
+    for k in range(bands):
         start_config=1+k*IFS_nconfigs
         for i in range(start_config,start_config+IFS_nconfigs-1): #start at 1 since new config is added after
             Full_MCE.InsertConfiguration(i, False) #insert new configuration with pickup from first configuration
@@ -71,7 +71,7 @@ if __name__ == '__main__':
                 current_op=Full_MCE.GetOperandAt(start_op_length+j)
                 current_op.GetOperandCell(i+1).Value = IFS_op.GetOperandCell(i-start_config+2).Value #set operand values to match existing K band MCE configurations
     
-    Full_System.SaveAs("C:\\Users\\mcosens\\Documents\\Zemax\\MIRMOS\\IFU\\MIRMOS_full_test.zos")
+    Full_System.SaveAs("C:\\Users\\mcosens\\Documents\\Zemax\\MIRMOS\\IFU\\MIRMOS_full_IFS.zmx") #save as zmx in order to make plotting functions work in 'make_IFU_plots.py'?
 
     # close server instance of OpticStudio
     del zos
